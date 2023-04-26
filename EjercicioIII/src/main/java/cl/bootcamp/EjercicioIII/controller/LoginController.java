bb2803f9b57a1818c89d29b8f13b37bd571aefef
mundenius <121872746+mundenius@users.noreply.github.com>
2023-04-25 23:17:34 -0400

    ajustes controllers y archivo web.xml, abre el login finally

--- EjercicioIII/src/main/java/cl/bootcamp/EjercicioIII/controller/LoginController.java
+++ EjercicioIII/src/main/java/cl/bootcamp/EjercicioIII/controller/LoginController.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import cl.bootcamp.EjercicioIII.model.ValidacionService;
 
 @Controller
-//@RequestMapping("/EjercicioIII")
 public class LoginController {
 
     @Autowired
@@ -20,15 +18,20 @@ public class LoginController {
     public String index() {
         return "redirect:/index";
     }
-
-    @RequestMapping("/validacionLogin")
-    public String validar(@RequestParam("usuario") String nombre, @RequestParam("pass") String password, Model modelo) {
-        if(validacionService.validacionDatos(nombre, password)) {
-            modelo.addAttribute("nombre", nombre);
-            return "inicio";
-        } else {
-            modelo.addAttribute("mensaje", "Nombre de usuario o contraseña incorrectos");
-            return "index";
-        }
+    
+    @RequestMapping("/index")
+    public String login(){
+    	return "index";
     }
+
+//    @RequestMapping("/validacionLogin")
+//    public String validar(@RequestParam("usuario") String nombre, @RequestParam("pass") String password, Model modelo) {
+//        if(validacionService.validacionDatos(nombre, password)) {
+//            modelo.addAttribute("nombre", nombre);
+//            return "inicio.jsp";
+//        } else {
+//            modelo.addAttribute("mensaje", "Nombre de usuario o contraseña incorrectos");
+//            return "index.jsp";
+//        }
+//    }
 }
