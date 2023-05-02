86c1a9959e70572650687c15ee317e537591e8a4
nanzum1 <adatos.fer@gmail.com>
2023-05-02 10:09:35 -0400

    dependencia V

--- EjercicioV/src/main/java/cl/bootcamp/EjercicioV/dao/ImplCapacitacionDAO.java
+++ EjercicioV/src/main/java/cl/bootcamp/EjercicioV/dao/ImplCapacitacionDAO.java
@@ -1,7 +1,7 @@
 package cl.bootcamp.EjercicioV.dao;
 
 import java.util.ArrayList;
-import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import cl.bootcamp.EjercicioV.interfaces.ICapacitacionDao;
 import cl.bootcamp.EjercicioV.model.Capacitacion;
