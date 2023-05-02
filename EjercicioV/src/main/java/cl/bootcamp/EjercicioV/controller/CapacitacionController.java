17fe60417bd754c03bed510b236a3d4c05e3a9c4
mundenius <121872746+mundenius@users.noreply.github.com>
2023-05-02 17:05:53 -0400

    ejercicioV y VI funcionando

--- EjercicioV/src/main/java/cl/bootcamp/EjercicioV/controller/CapacitacionController.java
+++ EjercicioV/src/main/java/cl/bootcamp/EjercicioV/controller/CapacitacionController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import cl.bootcamp.EjercicioV.interfaces.ICapacitacionDao;
 import cl.bootcamp.EjercicioV.dao.ImplCapacitacionDAO;
 import cl.bootcamp.EjercicioV.model.Capacitacion;
 
@@ -38,43 +39,44 @@ public class CapacitacionController {
 	
 	@RequestMapping(value="/listarCapacitaciones", method = RequestMethod.GET)
 	public ModelAndView listarCapacitaciones(HttpServletResponse response) throws IOException{
+		ICapacitacionDao capdao = new ImplCapacitacionDAO();
+		System.out.println(capdao.obtenerCapacitacion());
+		return new ModelAndView("listarCapacitaciones");
+//			// Creamos una lista de capacitaciones
+//			List<Map<String, String>> capacitaciones = new ArrayList<Map<String, String>>();
+//			
+//			// Creamos una capacitación
+//			Map<String, String> capacitacion1 = new HashMap<String, String>();
+//			capacitacion1.put("idcapacitacion", "1");
+//			capacitacion1.put("capfecha", "2023-05-15");
+//			capacitacion1.put("caphora", "10:00");
+//			capacitacion1.put("caplugar", "Santiago");
+//			capacitacion1.put("capduracion", "2 horas");
+//			capacitacion1.put("cliente_rut_cliente", "76.543.210-K");
+//			capacitacion1.put("capnombre", "Introducción a Spring Boot");
+//			capacitacion1.put("capasistentes", "20");
+//			
+//			// Agregamos la capacitación a la lista
+//			capacitaciones.add(capacitacion1);
+//			
+//			// Creamos otra capacitación
+//			Map<String, String> capacitacion2 = new HashMap<String, String>();
+//			capacitacion2.put("idcapacitacion", "2");
+//			capacitacion2.put("capfecha", "2023-05-20");
+//			capacitacion2.put("caphora", "14:00");
+//			capacitacion2.put("caplugar", "Viña del Mar");
+//			capacitacion2.put("capduracion", "3 horas");
+//			capacitacion2.put("cliente_rut_cliente", "99.876.543-2");
+//			capacitacion2.put("capnombre", "Introducción a HTML y CSS");
+//			capacitacion2.put("capasistentes", "15");
+//			
+//			// Agregamos la capacitación a la lista
+//			capacitaciones.add(capacitacion2);
+//			
+//			// Creamos el ModelAndView y le pasamos las capacitaciones como atributo
+//			ModelAndView modelAndView = new ModelAndView("listarCapacitaciones");
+//			modelAndView.addObject("capacitaciones", capacitaciones);
 			
-			// Creamos una lista de capacitaciones
-			List<Map<String, String>> capacitaciones = new ArrayList<Map<String, String>>();
-			
-			// Creamos una capacitación
-			Map<String, String> capacitacion1 = new HashMap<String, String>();
-			capacitacion1.put("idcapacitacion", "1");
-			capacitacion1.put("capfecha", "2023-05-15");
-			capacitacion1.put("caphora", "10:00");
-			capacitacion1.put("caplugar", "Santiago");
-			capacitacion1.put("capduracion", "2 horas");
-			capacitacion1.put("cliente_rut_cliente", "76.543.210-K");
-			capacitacion1.put("capnombre", "Introducción a Spring Boot");
-			capacitacion1.put("capasistentes", "20");
-			
-			// Agregamos la capacitación a la lista
-			capacitaciones.add(capacitacion1);
-			
-			// Creamos otra capacitación
-			Map<String, String> capacitacion2 = new HashMap<String, String>();
-			capacitacion2.put("idcapacitacion", "2");
-			capacitacion2.put("capfecha", "2023-05-20");
-			capacitacion2.put("caphora", "14:00");
-			capacitacion2.put("caplugar", "Viña del Mar");
-			capacitacion2.put("capduracion", "3 horas");
-			capacitacion2.put("cliente_rut_cliente", "99.876.543-2");
-			capacitacion2.put("capnombre", "Introducción a HTML y CSS");
-			capacitacion2.put("capasistentes", "15");
-			
-			// Agregamos la capacitación a la lista
-			capacitaciones.add(capacitacion2);
-			
-			// Creamos el ModelAndView y le pasamos las capacitaciones como atributo
-			ModelAndView modelAndView = new ModelAndView("listarCapacitaciones");
-			modelAndView.addObject("capacitaciones", capacitaciones);
-			
-			return modelAndView;
 		}
 	}
 
